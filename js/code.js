6d666a3ff83a946416ba77249706b3d92694bcdc
Asandile <asandilelangeni@gmail.com>
2022-11-08 13:11:37 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,7 @@
+let firstName = prompt("please enter your firstName");
+let surname = prompt("please enter your surname");
+let userSalary= prompt("please enter your salary")
+salaryBonus = eval(userSalary) + 500
+console.log(`Name: ${firstName}`);
+console.log(`Surname: ${surname}`);
+console.log(`salary: R${salaryBonus}`);
